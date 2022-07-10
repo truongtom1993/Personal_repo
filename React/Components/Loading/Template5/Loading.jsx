e33375eb7e8f9a837a80003386d05c6d9cb41ffa
TruongDN <dang_nhat_truong@hotmail.com>
2022-07-10 22:59:35 +0700

    Template5

---
+++ React/Components/Loading/Template5/Loading.jsx
@@ -0,0 +1,9 @@
+import './Loading.scss';
+
+export default function Loading() {
+	return (
+		<svg class='ring' viewBox='25 25 50 50' stroke-width='5'>
+			<circle cx='50' cy='50' r='20' />
+		</svg>
+	);
+}
