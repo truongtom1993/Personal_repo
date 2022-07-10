a381a9ad9c6e6acb3f71f7fab098bf8013376d00
TruongDN <dang_nhat_truong@hotmail.com>
2022-07-10 22:51:24 +0700

    Template 2

---
+++ React/Components/Template 2/Loading.jsx
@@ -0,0 +1,14 @@
+import './Loading.scss';
+
+export default function Loading(props) {
+	return (
+		<div class='gooey'>
+			<span class='dot'></span>
+			<div class='dots'>
+				<span></span>
+				<span></span>
+				<span></span>
+			</div>
+		</div>
+	);
+}
