96aede728e6a7805d7c3ea6099249ec161ae185a
TruongDN <dang_nhat_truong@hotmail.com>
2022-07-10 22:57:22 +0700

    template 4

---
+++ React/Components/Loading/Template4/Loading.jsx
@@ -0,0 +1,12 @@
+import './Loading.scss';
+
+export default function Loading() {
+	return (
+		<div class='metronome'>
+			<div class='metronome__dot'></div>
+			<div class='metronome__dot'></div>
+			<div class='metronome__dot'></div>
+			<div class='metronome__dot'></div>
+		</div>
+	);
+}
